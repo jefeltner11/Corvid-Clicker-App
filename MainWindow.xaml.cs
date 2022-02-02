0ac19bddd25a29617d214cab03ab21811ac61c64
James Feltner <jefeltner11@gmail.com>
2022-02-02 00:47:33 -0600

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using ClickerApp.Models;
+using System.Timers;
+
+namespace ClickerApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+
+    public partial class MainWindow : Window
+    {
+        Clicker corvidClicker = new Clicker();
+        Clicker ravenClicker = new Clicker();
+        private static System.Timers.Timer aTimer;
+        
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = corvidClicker;
+            SetTimer();
+        }
+
+
+        private void SetTimer()
+        {
+            Timer aTimer = new System.Timers.Timer(1000);
+            aTimer.Elapsed += OnTimedEvent;
+            aTimer.AutoReset = true;
+            aTimer.Enabled = true;
+        }
+        private void OnTimedEvent(Object source, ElapsedEventArgs e)
+        {
+            corvidClicker.ClickCount += corvidClicker.cpsTracker;
+        }
+        private void submitButton_Click(object sender, RoutedEventArgs e)
+        {
+            corvidClicker.ClickCount++;
+        }
+
+
+        private void buyBaitButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (corvidClicker.ClickCount >= corvidClicker.baitCost)
+            {
+                corvidClicker.ClickCount -= corvidClicker.baitCost;
+                corvidClicker.baitCount += 1;
+                corvidClicker.baitCost += 5 * corvidClicker.baitCount;
+                corvidClicker.cpsTracker += 1;
+            }
+            
+        }
+
+        private void buyCageButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (corvidClicker.ClickCount >= corvidClicker.cageCost)
+            {
+                corvidClicker.ClickCount -= corvidClicker.cageCost;
+                corvidClicker.cageCount += 1;
+                corvidClicker.cageCost += 50 * corvidClicker.cageCount;
+                corvidClicker.cpsTracker += 5;
+            }
+        }
+
+        private void buyAviaryButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (corvidClicker.ClickCount >= corvidClicker.aviaryCost)
+            {
+                corvidClicker.ClickCount -= corvidClicker.aviaryCost;
+                corvidClicker.aviaryCount += 1;
+                corvidClicker.aviaryCost += 250 * corvidClicker.aviaryCount;
+                corvidClicker.cpsTracker += 20;
+            }
+        }
+
+        private void buyMagnetButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (corvidClicker.ClickCount >= corvidClicker.magnetCost)
+            {
+                corvidClicker.ClickCount -= corvidClicker.magnetCost;
+                corvidClicker.magnetCount += 1;
+                corvidClicker.magnetCost += 1000 * corvidClicker.magnetCount;
+                corvidClicker.cpsTracker += 50;
+            }
+        }
+
+        private void buyDroneButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (corvidClicker.ClickCount >= corvidClicker.droneCost)
+            {
+                corvidClicker.ClickCount -= corvidClicker.droneCost;
+                corvidClicker.droneCount += 1;
+                corvidClicker.droneCost += 10000 * corvidClicker.droneCount;
+                corvidClicker.cpsTracker += 350;
+            }
+        }
+
+        private void buyEmpButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (corvidClicker.ClickCount >= corvidClicker.empCost)
+            {
+                corvidClicker.ClickCount -= corvidClicker.empCost;
+                corvidClicker.empCount += 1;
+                corvidClicker.empCost += 50000 * corvidClicker.empCount;
+                corvidClicker.cpsTracker += 1500;
+            }
+        }
+
+        private void buyFlareButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (corvidClicker.ClickCount >= corvidClicker.flareCost)
+            {
+                corvidClicker.ClickCount -= corvidClicker.flareCost;
+                corvidClicker.flareCount += 1;
+                corvidClicker.flareCost += 250000 * corvidClicker.flareCount;
+                corvidClicker.cpsTracker += 7500;
+            }
+        }
+
+        private void imageButton_MouseEnter(object sender, MouseEventArgs e)
+        {
+            imageButton.BorderBrush = Brushes.Black;
+            crowImage.Height += 10;
+            crowImage.Width += 10;
+        }
+
+        private void imageButton_MouseLeave(object sender, MouseEventArgs e)
+        {
+            imageButton.BorderBrush = Brushes.Transparent;
+            crowImage.Height -= 10;
+            crowImage.Width -= 10;
+        }
+
+        private void imageButton_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            corvidClicker.ClickCount += 1;
+            imageButton.Height -= 10;
+            imageButton.Width -= 10;
+            crowImage.Height -= 10;
+            crowImage.Width -= 10;
+        }
+
+        private void imageButton_PreviewMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            imageButton.Height += 10;
+            imageButton.Width += 10;
+            crowImage.Height += 10;
+            crowImage.Width += 10;
+        }
+    }
+}
---
+++ Models/ClickerClass.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ClickerApp.Models
+{
+    public class Clicker : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private int _clickCount = 0;
+        private int _cps = 0;
+        private int _baitCount = 0;
+        private int _baitCost = 10;
+        private int _cageCount = 0;
+        private int _cageCost = 100;
+        private int _aviaryCount = 0;
+        private int _aviaryCost = 750;
+        private int _magnetCount = 0;
+        private int _magnetCost = 5000;
+        private int _droneCount = 0;
+        private int _droneCost = 25000;
+        private int _empCount = 0;
+        private int _empCost = 100000;
+        private int _flareCount = 0;
+        private int _flareCost = 500000;
+
+        public int ClickCount
+        {
+            get { return _clickCount; }
+            set
+            {
+                _clickCount = value;
+                OnPropertyChanged();
+            }
+        }
+        public int cpsTracker
+        {
+            get { return _cps;  }
+            set
+            {
+                _cps = value;
+                OnPropertyChanged();
+            }
+        }
+        public int baitCount
+        {
+            get { return _baitCount; }
+            set
+            {
+                _baitCount = value;
+                OnPropertyChanged();
+            }
+        }
+        public int baitCost
+        {
+            get { return _baitCost; }
+            set
+            {
+                _baitCost = value;
+                OnPropertyChanged();
+            }
+        }
+        public int cageCount
+        {
+            get { return _cageCount; }
+            set
+            {
+                _cageCount = value;
+                OnPropertyChanged();
+            }
+        }
+        public int cageCost
+        {
+            get { return _cageCost; }
+            set
+            {
+                _cageCost = value;
+                OnPropertyChanged();
+            }
+        }
+        public int aviaryCount
+        {
+            get { return _aviaryCount; }
+            set
+            {
+                _aviaryCount = value;
+                OnPropertyChanged();
+            }
+        }
+        public int aviaryCost
+        {
+            get { return _aviaryCost; }
+            set
+            {
+                _aviaryCost = value;
+                OnPropertyChanged();
+            }
+        }
+        public int magnetCount
+        {
+            get { return _magnetCount; }
+            set
+            {
+                _magnetCount = value;
+                OnPropertyChanged();
+            }
+        }
+        public int magnetCost
+        {
+            get { return _magnetCost; }
+            set
+            {
+                _magnetCost = value;
+                OnPropertyChanged();
+            }
+        }
+        public int droneCount
+        {
+            get { return _droneCount; }
+            set
+            {
+                _droneCount = value;
+                OnPropertyChanged();
+            }
+        }
+        public int droneCost
+        {
+            get { return _droneCost; }
+            set
+            {
+                _droneCost = value;
+                OnPropertyChanged();
+            }
+        }
+        public int empCount
+        {
+            get { return _empCount; }
+            set
+            {
+                _empCount = value;
+                OnPropertyChanged();
+            }
+        }
+        public int empCost
+        {
+            get { return _empCost; }
+            set
+            {
+                _empCost = value;
+                OnPropertyChanged();
+            }
+        }
+        public int flareCount
+        {
+            get { return _flareCount; }
+            set
+            {
+                _flareCount = value;
+                OnPropertyChanged();
+            }
+        }
+        public int flareCost
+        {
+            get { return _flareCost; }
+            set
+            {
+                _flareCost = value;
+                OnPropertyChanged();
+            }
+        }
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
